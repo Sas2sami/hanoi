20d070ae75cfe2bd4d6c063b369be0179a16bc62
Sas2sami <weronikajucha@gmail.com>
2026-05-01 13:30:48 +0200

    Dodanie głównej logiki

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
-def main():
-    print("Hello World!")
+def hanoi(n, start, cel, pomocniczy):
+    if n == 1:
+        print(f"Przenieś dysk 1 z {start} do {cel}")
+    else:
+        hanoi(n - 1, start, pomocniczy, cel)
+        print(f"Przenieś dysk {n} z {start} do {cel}")
+        hanoi(n - 1, pomocniczy, cel, start)
 
-main()
+hanoi(3, 'A', 'C', 'B')
