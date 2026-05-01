8572a11cc2dae4a8e10fdef1b22b8a60a810e8b7
Sas2sami <weronikajucha@gmail.com>
2026-05-01 14:17:39 +0200

    Dodanie zasad ruchu / "Legalny ruch"

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
+stan = {
+    "A": [3, 2, 1],
+    "B": [],
+    "C": []
+}
 def hanoi(n, start, cel, pomocniczy):
     if n == 1:
         print(f"Przenieś dysk 1 z {start} do {cel}")
@@ -6,4 +11,12 @@ def hanoi(n, start, cel, pomocniczy):
         print(f"Przenieś dysk {n} z {start} do {cel}")
         hanoi(n - 1, pomocniczy, cel, start)
 
+def legalne_ruch(stan,start,cel):
+    if len(stan[start]) == 0:
+        return False
+    if len(stan[cel]) == 0:
+        return True
+    return stan[start][-1] < stan[cel][-1]
+
 hanoi(3, "A", "C", "B")
+print(stan)
