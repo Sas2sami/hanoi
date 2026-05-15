f576e232323f9510278b7a6e7bdcff7dcd9d1ae8
Sas2sami <weronikajucha@gmail.com>
2026-05-15 23:08:50 +0200

    BFS/DFS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
+from collections import deque
+import time
 
 stan = {
     "A": [3, 2, 1],
     "B": [],
     "C": []
 }
-
 start = ((3, 2, 1), (), ())
 goal = ((), (), (3, 2, 1))
 
@@ -45,10 +46,72 @@ def get_moves(state):
                 new_state = [list(rod) for rod in state]
                 new_state[j].append(new_state[i].pop())
                 moves.append(tuple(tuple(rod) for rod in new_state))
-
     return moves
 
+def bfs(start, goal):
+    start_time = time.perf_counter()
+    queue = deque([(start, [])])
+    visited = set()
+    visited_count = 0
+
+    while queue:
+        state, path = queue.popleft()
+
+        if state == goal:
+            duration = time.perf_counter() - start_time
+            return path, visited_count, duration
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+        visited_count += 1
+
+        for next_state in get_moves(state):
+            queue.append((next_state, path + [next_state]))
+
+    duration = time.perf_counter() - start_time
+    return None, visited_count, duration
+
+def dfs(start, goal):
+    start_time = time.perf_counter()
+    stack = [(start, [])]
+    visited = set()
+    visited_count = 0
+
+    while stack:
+        state, path = stack.pop()
+        if state == goal:
+            duration = time.perf_counter() - start_time
+            return path, visited_count, duration
+        
+        if state in visited:
+            continue
+        visited.add(state)
+        visited_count += 1
+
+        for next_state in get_moves(state):
+            stack.append((next_state, path + [next_state]))
+    
+    duration = time.perf_counter() - start_time
+    return None, visited_count, duration
 
 
+if __name__ == "__main__":
+    path, visited_count, duration = bfs(start, goal)
+    print("BFS")
+    print("Liczba ruchów:", len(path))
+    print("Liczba odwiedzonych stanów:", visited_count)
+    print("Czas działania:", f"{duration:.8f} s")
+    print("\nKolejno stany:")
+    for step in path:
+        print(step)
 
-
+    path, visited_count, duration = dfs(start, goal)
+    print("\nDFS")
+    print("Liczba ruchów:", len(path))
+    print("Liczba odwiedzonych stanów:", visited_count)
+    print("Czas działania:", f"{duration:.8f} s")
+    print("\nKolejno stany:")
+    for step in path:
+        print(step)
